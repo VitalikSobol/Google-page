659f8e5f4f79d09a0cb2ab05f57980f83bbb555c
Vitalik <vitaliysobol93@gmail.com>
2018-01-05 14:46:49 +0300

    Google page for Startup Winter

---
+++ js/script.js
@@ -0,0 +1,9 @@
+(function($){
+    $(document).ready(function(){
+       
+        $('#srch').on('click',function(){
+                $('.hidden-div').toggleClass('show');
+        });
+        
+    });
+})(jQuery);
